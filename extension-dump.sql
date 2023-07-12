4db2706f943b60a3127b4d9ce55851261239860b
Gruzin <grzegorz.jagielski.2019@zsa.pwr.edu.pl>
2023-07-13 00:20:59 +0200

    Deployment files - added nginx to stack

--- extension-dump.sql
+++ extension-dump.sql
@@ -9,3 +9,20 @@ ALTER TABLE ONLY public.results
     ADD CONSTRAINT results_pkey PRIMARY KEY (date);
 
 ALTER TABLE public.results OWNER TO postgres;
+
+CREATE TABLE public.users (
+    id SERIAL NOT NULL,
+    token text NOT NULL,
+    max_streak int DEFAULT 0,
+    current_streak int DEFAULT 0,
+    total_solves int DEFAULT 0,
+    last_solve date DEFAULT CURRENT_DATE - INTEGER '1' NOT NULL
+);
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_token_key UNIQUE (token);
+
+ALTER TABLE public.users OWNER TO postgres;
